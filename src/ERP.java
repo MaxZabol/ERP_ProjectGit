071a68950e0a73864dade6ff88cbc4d50abcd8c1
dilosh12 <dilora753@gmail.com>
2023-05-18 02:27:10 -0400

    Additional header messages created.

--- src/ERP.java
+++ src/ERP.java
@@ -1,7 +1,8 @@
 public class ERP {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Welcome to ERP");
+        System.out.println("Welcome to ERP!");
+        System.out.println("Gear up and get ready for a new project!");
     }
 
 }

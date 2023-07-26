04b237155ecca1e7e5fe76475725b3269debb686
Van Gogh <fatihtoplu@Fatihs-MacBook-Pro.local>
2023-07-26 21:53:04 +0200

    Added the solution of week9 by fatih.

--- src/fatih/Week8/NUniqueIntegersThatSumUpToZero.java
+++ src/fatih/Week8/NUniqueIntegersThatSumUpToZero.java
@@ -3,7 +3,8 @@ package fatih.Week8;
 public class NUniqueIntegersThatSumUpToZero {
 
     /*
-    Write a function that given an integer N (1 < N < 100), returns an array containing N unique integers that sum up to 0. The function can return any such array.
+    Write a function that given an integer N (1 < N < 100), returns an array containing N unique integers that sum up to 0.
+    The function can return any such array.
   Example 1:
     N = 4 => the function could return [1, 0, -3, 2] or [-2, 1, -4, 5].
 
